4805713d09e30b62ab84138bc49373dc1c262d4a
0-don <don.cryptus@gmail.com>
2026-04-13 00:55:38 +0200

    refactor: simplify QueryClient instantiation by removing default options

--- src/web/lib/react-query/client.ts
+++ src/web/lib/react-query/client.ts
@@ -1,15 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
 
 export function makeQueryClient(): QueryClient {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        staleTime: 10_000,
-        retry: 1,
-      },
-    },
-  });
+  return new QueryClient();
 }
 
 let browserQueryClient: QueryClient | undefined;
